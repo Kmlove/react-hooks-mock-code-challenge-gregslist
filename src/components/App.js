43fa53b3c468ae70844525616223d2093aa33c88
Kim Love <kimberlymlove15@gmail.com>
2023-08-30 19:35:30 -0400

    First deliverable. Listings render to page on intial load

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const url = "http://localhost:6001/listings"
+  const [ listings, setListings] = useState([])
+  
+  useEffect(()=> {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setListings(data))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
